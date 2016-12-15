d095edfcbcf49bf08b06c09be0930b94505e2f89
Etienne Laurin <etienne@atnnn.com>
2016-12-15 11:24:59 +0000

    added readme, centos, reports, package download links, tgz, license and more

--- README.md
+++ README.md
@@ -1,5 +1,64 @@
-# RethinkDB CI
+# Continuous integration for RethinkDB
 
-Currently being tested at http://thanos.atnnn.com:3000
+These scripts run on my [Thanos](http://thanos.atnnn.com:3000) build
+server, allowing us to build RethinkDB, run the tests and build
+packages.
 
-Based on [Hydra](http://nixos.org/hydra/manual/).
+To add your own server to the build farm contact me at
+etienne@atnnn.com. I need macOS, Windows and Linux (PC and ARM)
+machines to extend the test coverage and add packages for Windows, for
+macOS and for other Linux distributions. I would also like to run the
+performance tests.
+
+## Download Packages
+
+Thanos currently builds packages for Ubuntu and Centos. I will try to
+add more packages if there is any demand.
+
+Build artifacts and packages haven't been organised yet, but most of
+them are available.
+
+* Open the RethinkDB project page at
+  http://thanos.atnnn.com:3000/project/rethinkdb
+
+* Click on the branch you want to download, and locate the *Jobs*
+  tab. For example, the `next` branch:
+  http://thanos.atnnn.com:3000/jobset/rethinkdb/next#tabs-jobs
+
+* Click the green checkbox next to the package you want to open the
+  build page
+
+* Locate the download links in the Build Products section
+
+# Contributing
+
+## Local Setup
+
+Running these scripts locally requires [Nix](http://nixos.org/nix/).
+
+Checkout RethinkDB and RethinkDB-Nix side-by-side:
+
+```
+git clone https://github.com/rethinkdb/rethinkdb
+git clone https://github.com/atnnn/rethinkdb-nix
+```
+
+Syntax-check the scripts:
+
+```
+cd rethinkdb-nix
+nix-instantiate -I .. release.nix
+```
+
+Build a RethinkDB source tarball:
+
+```
+nix-build -I .. release.nix -A sourceTgz
+```
+
+`nix-build` parameters that can help debug errors are:
+
+* `-K` - keep the build folder on failure
+* `-v`, `-vv`, ... - be more verbose
+* `--show-trace` - stack traces for nix errors
+
--- release.nix
+++ release.nix
@@ -1,8 +1,13 @@
+# TODO link from configuration.nix to here:
+#  - save core dumps
+#  - increase process count limit and fd limit for builders
+
 with rec {
   inherit (builtins)
     toFile readFile concatStringsSep unsafeDiscardStringContext
     listToAttrs match head tail toJSON toPath elemAt getAttr length
-    trace genList isString fromJSON getEnv concatLists toString;
+    trace genList isString fromJSON getEnv concatLists toString
+    isAttrs;
   foldl = builtins.foldl'; #'
   tracing = x: trace x x;
   for = xs: f: map f xs;
@@ -27,7 +32,7 @@ let
     done
     '';
 
-  make = "make -j $NIX_BUILD_CORES -l $NIX_BUILD_CORES";
+  make = "make -j $NIX_BUILD_CORES";
 
   skip_tests = [
     # known failures (TODO: fix upstream)
@@ -62,13 +67,17 @@ let
   depInfo = dep: let
     source = readFile (toPath "${<rethinkdb>}/mk/support/pkg/${dep}.sh");
     find = var: let go = source: val:
-      let group = match "(.*?)\n *${var}=([^\n]*)\n.*" source;
+      let group = match "(.*?)\n *${var}=\"?([^\"\n]*)\"?\n.*" source;
       in if group == null then val else go (head group) (elemAt group 1);
     in go source null;
   in rec {
     version = replace "/-patched.*/" "" (find "version");
     url = replace "\\$\\{?version([^}]*})?" version (find "src_url");
     sha1 = find "src_url_sha1";
+  };
+
+  stripForFetching = dep: src: pkgs.stdenv.mkDerivation {
+    # TODO
   };
 
   mkFetch = fetch_list: pkgs.stdenv.mkDerivation (rec {
@@ -79,17 +88,26 @@ let
     buildInputs = [ <rethinkdb> ];
   } // listToAttrs (map (dep: let info = depInfo dep; in {
     name = dep;
-    value = pkgs.stdenv.mkDerivation {
-      name = "rethinkdb-fetch-${dep}-${info.version}";
-      src = pkgs.fetchurl {
-        url = info.url;
-        sha1 = info.sha1;
+    value = if info.url != null
+      then pkgs.stdenv.mkDerivation {
+        name = "rethinkdb-fetch-${dep}-${info.version}";
+        src = pkgs.fetchurl {
+          url = info.url;
+          sha1 = info.sha1;
+        };
+        builder = mkBuilder ''
+          mkdir -p $out/external/.cache
+          ln -s $src $out/external/.cache/''${src#*-}
+        '';
+      } else pkgs.stdenv.mkDerivation {
+	name = "rethinkdb-fetch-${dep}-${info.version}";
+	src = stripForFetching dep <rethinkdb>;
+	builder = mkBuilder ''
+	  ${src.fetch}
+	  mkdir $out
+	  cp -r external/${dep}_* $out/
+	'';
       };
-      builder = mkBuilder ''
-        mkdir -p $out/external/.cache
-        ln -s $src $out/external/.cache/''${src#*-}
-      '';
-    };
   }) fetch_list));
 
   fetchList = ["v8" "jemalloc"];
@@ -104,6 +122,33 @@ let
     "libssl-dev"
     "devscripts" "debhelper" "fakeroot"
   ];
+
+  rpmExtraRepos = {
+    centos6 = arch: {
+      url = "http://linuxsoft.cern.ch/cern/devtoolset/slc6X/${arch}/yum/devtoolset/repodata/primary.xml.gz";
+      prefix = "http://linuxsoft.cern.ch/cern/devtoolset/slc6X/${arch}/yum/devtoolset";
+      hash = getAttr arch {
+	i686 = "0m6wkm96435j4226cgf3x0p7z6gmdjgw3mrd2ybd4s5v3w648sgx";
+	x86_64 = "0zp7q8aayjk677d40hxby0lz2zwd2jwifz8jca4pli2k5gv4mbj5";
+      };
+    };
+    centos7 = {
+      url = "https://dl.fedoraproject.org/pub/epel/7/x86_64/repodata/8b36e7f48bd4a63b0b5a4be88c0202a35d2092a753ac887035c47aa5769317ad-primary.xml.gz";
+      prefix = "https://dl.fedoraproject.org/pub/epel/7/x86_64";
+      hash = "1b8pjdvaayn46mq8ib2kly920pd30818rs2bb85kp9nligsffdlb";
+    };
+  };
+
+  rpmBuildDeps = rec {
+    common = [ "boost-static" "tar" "which" "m4" ];
+    centos6 = common ++ [ "devtoolset-2" ];
+    centos7 = common ++ [
+      # centos
+      "git" "gcc-c++" "ncurses-devel" "make" "ncurses-static" "zlib-devel" "zlib-static"
+      # epel
+      "protobuf-devel" "protobuf-static" "jemalloc-devel"
+    ];
+  };
 
   sourcePrep = pkgs.stdenv.mkDerivation rec {
     name = "rethinkdb-${version}";
@@ -148,6 +193,19 @@ let
     ];
   };
 
+  sourceTgz = pkgs.stdenv.mkDerivation rec {
+    name = "rethinkdb-${src.version}-source";
+    src = sourcePrep;
+    buildInputs = [ pkgs.gnutar ];
+    builder = mkBuilder ''
+      mkdir $out
+      cd $src
+      tar --transform 's|^.|rethinkdb-${src.version}|' -zcf $out/rethinkdb-${src.version}.tgz ./
+      mkdir $out/nix-support
+      echo file source-dist $out/rethinkdb-${src.version}.tgz > $out/nix-support/hydra-build-products
+    '';
+  };
+
   fastTests = pkgs.stdenv.mkDerivation rec {
     name = "rethinkdb-fast-tests-results-${src.version}";
     src = sourcePrep;
@@ -161,17 +219,26 @@ let
       openssl
       boost
       curl
+      git # TODO
     ];
     builder = mkBuilder ''
       cp -r $src/* .
       chmod -R u+w .
-      ${patchScripts}
+      patchShebangs .
+
+      # TODO upstream remove dependency on git
+      git init
+      git config user.email joe@example.com
+      git config user.name Joe
+      git commit --allow-empty -m "empty"
+      
       ./configure
-      ${make} DEBUG=1
-      test/run -j $NIX_BUILD_CORES '!long' ${skip_tests_filter} || :
+      ${make} DEBUG=1 
+      mkdir -p $out/nix-support
+      test/run -H -j $((NIX_BUILD_CORES / 2)) unit cpplint ${skip_tests_filter} || touch $out/nix-support/failed
       test -e test/results/*/test_results.html
-      cp test/results/* $out
-      echo report test_results $out/*/test_results.html > $out/nix-support/hydra-build-products
+      cp -r test/results/* $out
+      echo report html $out/*/test_results.html > $out/nix-support/hydra-build-products
     '';
   };
 
@@ -189,14 +256,85 @@ let
       inherit diskImage;
     });
 
-  debs = with pkgs.vmTools.diskImageFuns;
+  # TODO: move missing distros upstream
+  customDebDistros = with pkgs; with vmTools; with debDistros; debDistros // {
+    ubuntu1610i386 = {
+      name = "ubuntu-16.10-yakkety-i386";
+      fullName = "Ubuntu 16.10 Yakkety (i386)";
+      packagesLists = [
+	(fetchurl {
+	   url = mirror://ubuntu/dists/yakkety/main/binary-i386/Packages.xz;
+           sha256 = "13r75sp4slqy8w32y5dnr7pp7p3cfvavyr1g7gwnlkyrq4zx4ahy"; # TODO
+        })
+        (fetchurl {
+          url = mirror://ubuntu/dists/yakkety/universe/binary-i386/Packages.xz;
+          sha256 = "14fid1rqm3sc0wlygcvn0yx5aljf51c2jpd4x0zxij4019316hsh"; # TODO
+        })
+      ];
+      urlPrefix = mirror://ubuntu;
+      packages = commonDebPackages ++ [ "diffutils" "libc-bin" ];
+    };
+    ubuntu1610x86_64 = {
+      name = "ubuntu-16.10-yakkety-amd64";
+      fullName = "Ubuntu 16.10 Yakkety (amd64)";
+      packagesList = [
+        (fetchurl {
+          url = mirror://ubuntu/dists/yakkety/main/binary-amd64/Packages.xz;
+          sha256 = "1lg3s8fhip14k423k5scn9iqya775sr7ikbkqisppxypn3x4qv1m";
+        })
+        (fetchurl {
+          url = mirror://ubuntu/dists/yakkety/universe/binary-amd64/Packages.xz;
+          sha256 = "1rjpa3miq28p9ag05l9k9w5sr1skkcjwca4k7f79gmpzzvw609m7";
+        })
+      ];
+      urlPrefix = mirror://ubuntu;
+      packages = commonDebPackages ++ [ "diffutils" "libc-bin" ];
+    };
+  };
+
+  addRPMRepo = repo: distro: distro // {
+    packagesLists = (distro.packagesLists or [ distro.packagesList ]) ++ [ (
+      pkgs.fetchurl {
+	url = repo.url;
+	sha256 = repo.hash;
+      }
+    ) ];
+    urlPrefixes = (distro.urlPrefixes or [ distro.urlPrefix ]) ++ [ repo.prefix ];
+  };
+  customRpmDistros = with pkgs; with vmTools; with rpmDistros; rpmDistros // {
+    centos65i686 = addRPMRepo (rpmExtraRepos.centos6 "i686") centos65i686;
+    centos65x86_64 = addRPMRepo (rpmExtraRepos.centos6 "x86_64") centos65x86_64;
+    centos71x86_64 = addRPMRepo rpmExtraRepos.centos7 centos71x86_64;
+  };
+
+  # copied from nixpkgs
+  diskImageFuns =
+    (pkgs.lib.mapAttrs (name: as: as2: pkgs.vmTools.makeImageFromDebDist (as // as2)) customDebDistros)
+    // (pkgs.lib.mapAttrs (name: as: as2: pkgs.vmTools.makeImageFromRPMDist (as // as2)) customRpmDistros);
+
+  debs = with diskImageFuns;
     listToAttrs (concatLists (for [
+      { name = "yakkety";
+        b64 = ubuntu1610x86_64;
+	b32 = ubuntu1610i386; }
       { name = "xenial";
         b64 = ubuntu1604x86_64;
 	b32 = ubuntu1604i386; }
       { name = "wily";
 	b64 = ubuntu1510x86_64;
 	b32 = ubuntu1510i386; }
+      { name = "vivid";
+	b64 = ubuntu1504x86_64;
+	b32 = ubuntu1504i386; }
+      { name = "utopic";
+	b64 = ubuntu1410x86_64;
+	b32 = ubuntu1410i386; }
+      { name = "trusty";
+	b64 = ubuntu1404x86_64;
+	b32 = ubuntu1404i386; }
+      { name = "saucy";
+	b64 = ubuntu1310x86_64;
+	b32 = ubuntu1310i386; }
     ] ({ name, b64, b32, extra ? [] }: let
       dsc = vmBuild {
         name = "rethinkdb-${sourcePrep.version}-${name}-src";
@@ -205,8 +343,16 @@ let
           cp -r $src rethinkdb
           cd rethinkdb
           ./configure
-          make build-deb-src UBUNTU_RELEASE=${name} SIGN_PACKAGE=0
+
+          # TODO: upstream add -w to *.pb.cc build rule
+          ${if name == "yakkety" then "sed -i 's/^ALLOW_WARNINGS.*/ALLOW_WARNINGS = 1/' mk/defaults.mk" else ""}
+
+          make -j 6 build-deb-src UBUNTU_RELEASE=${name} SIGN_PACKAGE=0
           cp build/packages/*.{dsc,build,changes,tar.?z} $out
+          mkdir $out/nix-support
+          for p in $out/*.*; do
+            echo file deb-source "$p" >> $out/nix-support/hydra-build-products
+          done
         '';
         memSize = 4096;
         diskImage = b64 { extraPackages = debBuildDeps ++ extra; };
@@ -221,6 +367,10 @@ let
           cd /build/source
           debuild -b -us -uc -j6
           cp ../*.deb $out
+          mkdir $out/nix-support
+          for deb in $out/*.deb; do
+            echo file deb "$deb" >> $out/nix-support/hydra-build-products
+          done
         '';
         memSize = 8192;
         diskImage = diskImage {extraPackages = debBuildDeps ++ extra; };
@@ -231,6 +381,53 @@ let
       { name = "${name}-amd64"; value = deb "amd64" b64; }
     ])));
 
-in {
-  inherit sourcePrep fetchDependencies fastTests;
-} // debs # // rpms
+  rpms = with diskImageFuns;
+    listToAttrs (for [
+      { name = "centos7";
+	arch = "x86_64";
+        image = centos71x86_64; }
+      { name = "centos6";
+	arch = "x86_64";
+        image = centos65x86_64; }
+      { name = "centos6";
+	arch = "i686";
+        image = centos65i686; }
+    ] ({ name, arch, image, extra ? [] }: let
+      rpm = vmBuild {
+        name = "rethinkdb-${sourcePrep.version}-${name}-${arch}";
+        attrs = {
+	  src = sourcePrep;
+	  fpm = pkgs.fpm;
+          extraFetched = mkFetch [ "openssl" "curl" "libidn" "zlib" ];
+	};
+	build = ''
+          PATH=$fpm/bin:/usr/bin:/bin:/usr/sbin:/sbin
+	  cp -r $src /build
+          cp -r $extraFetched/* /build/
+          cd /build
+
+          # TODO: conditionally `--fetch all' upstream
+          # TODO: centos is missing krb5-static: https://bugzilla.redhat.com/show_bug.cgi?id=838782
+          ./configure --static all --prefix=/usr --sysconfdir=/etc --localstatedir=/var \
+              --fetch openssl --fetch crypto --fetch curl --fetch zlib --fetch libidn
+	  export NOCONFIGURE=1
+
+          scripts/build-rpm.sh
+	  cp build/release/rethinkdb.debug build/packages/rethinkdb*.rpm $out
+          mkdir $out/nix-support
+          echo file debug-symbols $out/rethinkdb.debug >> $out/nix-support/hydra-build-products
+          for rpm in $out/*.rpm; do
+            echo file rpm "$rpm" >> $out/nix-support/hydra-build-products
+          done
+        '';
+        memSize = 8192;
+        diskImage = image { extraPackages = getAttr name rpmBuildDeps; };
+      };
+    in { name = "${name}-${arch}"; value = rpm; }));
+
+in ({
+  inherit sourcePrep fetchDependencies fastTests sourceTgz;
+} // debs // rpms
+).TODO or {
+  inherit fastTests;
+}
