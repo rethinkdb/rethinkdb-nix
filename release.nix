d7fec21eddeb983f1068ce5e598c5486410d11ca
Etienne Laurin <etienne@atnnn.com>
2018-04-01 23:53:37 +0000

    remove references to deleted versions

--- release.nix
+++ release.nix
@@ -9,15 +9,10 @@
 
   # packages
   jessie-amd64 jessie-i386 jessie-src
-  saucy-amd64 saucy-i386 saucy-src
   stretch-amd64 stretch-i386 stretch-src
   trusty-amd64 trusty-i386 trusty-src
-  utopic-amd64 utopic-i386 utopic-src
-  vivid-amd64 vivid-i386 vivid-src
-  wily-amd64 wily-i386 wily-src
   xenial-amd64 xenial-i386 xenial-src
-  yakkety-amd64 yakkety-i386 yakkety-src
-  zesty-amd64 zesty-i386 zesty-src
+  artful-amd64 artful-i386 artful-src
   centos6-x86_64 centos7-x86_64
   ;
 }
